bcb10693365c52b1e54257ebeba1bbd314924c2a
caamanoluismiguel <caamano.luismiguel@gmail.com>
2025-12-29 13:33:51 -0500

    Delete js/script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function showTime() {
-  const timeElem = document.getElementById('currentTime');
-  if (timeElem) {
-    timeElem.innerHTML = new Date().toUTCString();
-  }
-}
-showTime();
-setInterval(showTime, 1000);
